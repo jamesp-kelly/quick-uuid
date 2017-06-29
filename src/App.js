227381e23f9dd1498dd228fff950290d98a2bbf1
James Kelly <jk_jameskelly@outlook.com>
2017-06-29 19:28:56 -0400

    add refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { v4 } from 'uuid';
 
-const App = () => <div>{v4()}</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.selectInput = this.selectInput.bind(this);
+  }
+  selectInput() {
+    this.textInput.select();
+  }
+  
+  render() {
+    return (
+      <div id="form-wrapper">
+        <input 
+          type="text" 
+          value={v4()}
+          spellCheck="false"
+          readOnly="true"
+          ref={(input) => {this.textInput = input; }}
+          onClick={this.selectInput}  />
+        <div>
+          <a href="/">refresh</a>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default App;
